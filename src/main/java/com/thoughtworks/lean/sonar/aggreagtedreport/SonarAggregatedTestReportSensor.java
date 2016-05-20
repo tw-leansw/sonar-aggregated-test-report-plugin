c3b4b375fa811708bc6c6fa8676b9174f98bbedb
qmxie <qmxie@thoughtworks.com>
2016-05-20 13:51:23 +0800

    [#1]  增加异常处理

--- src/main/java/com/thoughtworks/lean/sonar/aggreagtedreport/SonarAggregatedTestReportSensor.java
+++ src/main/java/com/thoughtworks/lean/sonar/aggreagtedreport/SonarAggregatedTestReportSensor.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.lean.sonar.aggreagtedreport;
 
+import com.thoughtworks.lean.sonar.aggreagtedreport.exception.LeanPluginException;
 import com.thoughtworks.lean.sonar.aggreagtedreport.scanner.ReportScanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sonar.api.batch.Sensor;
 import org.sonar.api.batch.SensorContext;
 import org.sonar.api.batch.fs.FileSystem;
@@ -12,6 +15,7 @@ public class SonarAggregatedTestReportSensor implements Sensor {
 
     private Settings settings;
     private FileSystem fileSystem;
+    private Logger LOGGER = LoggerFactory.getLogger(getClass());
 
 
     public SonarAggregatedTestReportSensor(FileSystem fileSystem, Settings settings) {
@@ -20,8 +24,13 @@ public class SonarAggregatedTestReportSensor implements Sensor {
     }
 
     public void analyse(Project project, SensorContext sensorContext) {
-        ReportScanner scanner = new ReportScanner(this.settings,this.fileSystem);
-        scanner.scanReport(project);
+        try {
+            ReportScanner scanner = new ReportScanner(this.settings, this.fileSystem);
+            scanner.scanReport(project);
+        }catch (LeanPluginException e){
+            LOGGER.warn("Make sure add -Dlean.aggregated.test.project.build is set.");
+            return;
+        }
     }
 
     public boolean shouldExecuteOnProject(Project project) {
