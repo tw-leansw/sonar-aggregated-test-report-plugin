9c0c59e8d01437b5d142f7b359d33a9f5136cbef
YongliuLi <ylli@thoughtworks.com>
2016-05-25 11:40:58 +0800

    [#0] set max scenario name  length to 100

--- src/main/resources/db/migration/V1_1__Create_lean_tables.sql
+++ src/main/resources/db/migration/V1_1__Create_lean_tables.sql
@@ -25,7 +25,7 @@ CREATE TABLE `lean_test_feature` (
 CREATE TABLE `lean_test_scenario` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `feature_id` INT NULL,
-  `name` VARCHAR(45) NULL,
+  `name` VARCHAR(100) NULL,
   `result_type` VARCHAR(45) NULL,
   `duration` INT NULL,
   PRIMARY KEY (`id`));
